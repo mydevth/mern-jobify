277e507892e55c8ffafc7381e064004d8f50954f
mydevth <mydevth@chonburi.biz>
2022-08-26 19:42:27 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import Landing from "./pages/Landing";
 import styled from "styled-components";
 
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 1rem;
-`;
-
-const ButtonSecond = styled.button`
-  background: blue;
-  color: white;
-  font-size: 1rem;
-`;
-
 function App() {
   return (
     <div>
