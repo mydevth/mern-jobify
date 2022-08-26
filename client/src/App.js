dfd279390cc2ceed1ab94eadf257b717fbfd1e60
mydevth <mydevth@chonburi.biz>
2022-08-26 22:27:09 +0700

    done section 7

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import Landing from "./pages/Landing";
-import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Register, Error, Landing } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 // eslint-disable-next-line
