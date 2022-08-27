54b3540ef555995505d9910381e911232cb10ae8
mydevth <mydevth@chonburi.biz>
2022-08-28 00:53:00 +0700

    section12

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,7 +2,7 @@ const createJob = async (req, res) => {
   res.send("create job");
 };
 const getAllJobs = async (req, res) => {
-  res.send("get All josb");
+  res.send("get All job");
 };
 const updateJob = async (req, res) => {
   res.send("update job");
--- models/User.js
+++ models/User.js
@@ -4,27 +4,38 @@ import validator from "validator";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please provide name"],
+    required: [true, "Please provide name"],
     minlength: 3,
-    max: 20,
+    maxlength: 20,
     trim: true,
   },
-  lastName: { type: String, maxLength: 20, trim: true, default: "lastName" },
   email: {
     type: String,
-    require: [true, "Please provide email"],
-    validate:{
+    required: [true, "Please provide email"],
+    validate: {
       validator: validator.isEmail,
-      message:'Please provide a valid email'
+      message: "Please provide a valid email",
     },
     unique: true,
   },
   password: {
     type: String,
-    require: [true, "Please provide password"],
+    required: [true, "Please provide password"],
     minlength: 6,
+    select: false,
   },
-  location: { type: String, trim: true, maxLength: 20, default: "my city" },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
 });
 
 export default mongoose.model("User", UserSchema);
