0f3600cc4353e5967c6a868537cc5bd4724118f8
mydevth <mydevth@chonburi.biz>
2022-08-28 23:14:02 +0700

    section14

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,26 @@
 import User from "../models/User.js";
-import {StatusCodes} from 'http-status-codes'
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.OK).json({ user });
 };
 const login = async (req, res) => {
   res.send("login user");
 };
 const updateUser = async (req, res) => {
   res.send("update User");
+ 
 };
 
 export { register, login, updateUser };
